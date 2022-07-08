a3f6d4a70fc7fe753d4a5855acd4889c0397e0ff
glezmoren <blancagonmor@gmail.com>
2022-07-08 12:28:05 +0200

    js evento enter

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict'
+
+let nodoEntrada = document.querySelector ( '#entrada');
+
+nodoEntrada.addEventListener( 'keyup', function(info_evento){
+    console.log('Pulso la tecla')
+    console.log(info_evento)
+    if( info_evento.key === 'Enter' ){ 
+        // Desencadena el pintado en la web
+        console.log( nodoEntrada.value );
+        
+        let nodoNombre = document.querySelector('.sorteo__nombre')
+        nodoNombre.innerHTML += `<div> ${nodoEntrada.value} </div>`
+
+        nodoEntrada.value = "";
+    }   
+});
